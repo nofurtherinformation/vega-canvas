86920825eda1b9076ac14ea31fa25774ad968587
Dylan <dhalpern@uchicago.edu>
2021-09-28 08:03:45 -0500

    Dom only

--- src/nodeCanvas.js
+++ src/nodeCanvas.js
@@ -1,28 +1,28 @@
-var NodeCanvas;
+// var NodeCanvas;
 
-['canvas', 'canvas-prebuilt'].some(function(libName) {
-  try {
-    NodeCanvas = require(libName);
-    if (typeof NodeCanvas !== 'function') {
-      NodeCanvas = null;
-    }
-  } catch (error) {
-    NodeCanvas = null;
-  }
-  return NodeCanvas;
-});
+// ['canvas', 'canvas-prebuilt'].some(function(libName) {
+//   try {
+//     NodeCanvas = require(libName);
+//     if (typeof NodeCanvas !== 'function') {
+//       NodeCanvas = null;
+//     }
+//   } catch (error) {
+//     NodeCanvas = null;
+//   }
+//   return NodeCanvas;
+// });
 
-export function nodeCanvas(w, h) {
-  if (NodeCanvas) {
-    try {
-      return new NodeCanvas(w, h);
-    } catch (e) {
-      // do nothing, return null on error
-    }
-  }
-  return null;
-}
+// export function nodeCanvas(w, h) {
+//   if (NodeCanvas) {
+//     try {
+//       return new NodeCanvas(w, h);
+//     } catch (e) {
+//       // do nothing, return null on error
+//     }
+//   }
+//   return null;
+// }
 
-export function nodeImage() {
-  return (NodeCanvas && NodeCanvas.Image) || null;
-}
+// export function nodeImage() {
+//   return (NodeCanvas && NodeCanvas.Image) || null;
+// }
